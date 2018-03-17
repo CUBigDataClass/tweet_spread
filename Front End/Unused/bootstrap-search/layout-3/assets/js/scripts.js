99bee501bd328caad1c6f80fe456b19396487302
Sharan Srivatsa <shsr7814@colorado.edu>
2018-03-17 16:21:29 -0600

    got it working tilll re

---
+++ Front End/Unused/bootstrap-search/layout-3/assets/js/scripts.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+	
+    /*
+        Background slideshow
+    */
+    $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Wow
+    */
+    new WOW().init();
+    
+    /*
+        Search form
+    */
+    $('.navbar-search-button .search-button').on('click', function(e){
+    	e.preventDefault();
+    	$(this).blur();
+    	$('.navbar-menu-items, .navbar-search-form').toggleClass('disabled');
+    	$('.navbar-search-form input.search').val('').focus();
+    });
+	
+});
