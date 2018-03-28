9ee0a1192f2ded89296c3beb4c51a83d48d8f3c5
Chu-Sheng Ku <bamboo983@gmail.com>
2018-03-28 21:54:23 +0000

    Get storm by shell script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,3 +7,8 @@ wget http://mirrors.gigenet.com/apache/zookeeper/zookeeper-3.4.11/zookeeper-3.4.
 tar -zxf zookeeper-3.4.11.tar.gz
 mv zookeeper-3.4.11 zookeeper
 rm zookeeper-3.4.11.tar.gz
+
+wget http://mirrors.gigenet.com/apache/storm/apache-storm-1.2.1/apache-storm-1.2.1.tar.gz
+tar -zxf apache-storm-1.2.1.tar.gz
+mv apache-storm-1.2.1 storm
+rm apache-storm-1.2.1.tar.gz
