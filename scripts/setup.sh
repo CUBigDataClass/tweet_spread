2170d2989e1de1feeb19159c049bc49fb26f2c54
Chu-Sheng Ku <bamboo983@gmail.com>
2018-03-29 20:40:26 +0000

    Get kafka by shell script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,17 @@
-# Install git
-sudo yum install git
-# Clone our code
-git clone https://github.com/CUBigDataClass/tweet_spread.git
-# Download Zookeeper
-wget http://mirrors.gigenet.com/apache/zookeeper/zookeeper-3.4.11/zookeeper-3.4.11.tar.gz
+# Download ZooKeeper
+wget http://download.nextag.com/apache/zookeeper/zookeeper-3.4.11//zookeeper-3.4.11.tar.gz
 tar -zxf zookeeper-3.4.11.tar.gz
 mv zookeeper-3.4.11 zookeeper
 rm zookeeper-3.4.11.tar.gz
 
-wget http://mirrors.gigenet.com/apache/storm/apache-storm-1.2.1/apache-storm-1.2.1.tar.gz
+# Download Storm
+wget http://mirror.cc.columbia.edu/pub/software/apache/storm/apache-storm-1.2.1/apache-storm-1.2.1.tar.gz
 tar -zxf apache-storm-1.2.1.tar.gz
 mv apache-storm-1.2.1 storm
 rm apache-storm-1.2.1.tar.gz
+
+# Download Kafka
+wget http://mirrors.sorengard.com/apache/kafka/1.1.0/kafka-1.1.0-src.tgz
+tar -zxf kafka-1.1.0-src.tgz
+mv kafka-1.1.0-src kafka
+rm kafka-1.1.0-src.tgz
