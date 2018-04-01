f57c07e9375731b958df8d9af30481f9c166c06a
Chu-Sheng Ku <bamboo983@gmail.com>
2018-04-01 17:32:00 -0600

    Updata Java 8 and install Maven

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,3 +15,12 @@ wget http://apache.mirrors.lucidnetworks.net/kafka/1.1.0/kafka_2.11-1.1.0.tgz
 tar -zxf kafka_2.11-1.1.0.tgz
 mv kafka_2.11-1.1.0 kafka
 rm kafka_2.11-1.1.0.tgz
+
+# Update Java 8
+sudo yum install java-1.8.0-openjdk-devel
+sudo yum remove java-1.7.0-openjdk
+
+# Install Maven
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
