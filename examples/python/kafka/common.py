78f070b12c5a6694d20e2b7cf48b11e8818f9015
amruta <hail.radical@gmail.com>
2018-04-07 16:24:00 -0600

    fixed import

--- examples/python/kafka/common.py
+++ examples/python/kafka/common.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 
-from kafka.structs import *
-from kafka.errors import *
+from examples.python.kafka.structs import *
+from examples.python.kafka.errors import *
