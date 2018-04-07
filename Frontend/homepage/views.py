49d93a066c85b9a2504f4fd6774dacd813c96d1a
CUBigDataClass <hail.radical@gmail.com>
2018-04-07 19:46:00 +0000

    [Add] add the log into django

--- Frontend/homepage/views.py
+++ Frontend/homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from . import process_search
-
+import logging
+log = logging.getLogger(__name__)
 
 
 def index(request):
@@ -11,6 +12,7 @@ def home(request):
 	if request.method == 'GET':
 		query = request.GET['search']
 		if query:
+                        log.debug("Hey there it works!!")
 			top_tweets_html = process_search.accept_input_for_processing(query)
 			sentiment = process_search.get_sentiment()
 			return render(request, 'homepage/search.html', {'query': query, 'top_tweets_html': top_tweets_html,
