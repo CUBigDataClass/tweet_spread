6769768ca1610e4ce32abcccd38c17fcd5f635d7
amruta <hail.radical@gmail.com>
2018-04-07 12:14:28 -0600

    reverted produce

--- Frontend/homepage/views.py
+++ Frontend/homepage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . import process_search
-from examples.python.producer import produce
+
 
 
 def index(request):
@@ -11,7 +11,6 @@ def home(request):
 	if request.method == 'GET':
 		query = request.GET['search']
 		if query:
-			produce(query)
 			top_tweets_html = process_search.accept_input_for_processing(query)
 			return render(request, 'homepage/search.html', {'query': query, 'top_tweets_html': top_tweets_html})
 
