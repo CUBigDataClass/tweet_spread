1d2dce969def09b2eb3abe5f7b1fe546f091d6b2
amruta <hail.radical@gmail.com>
2018-04-07 17:04:52 -0600

    fix

--- examples/python/kafka/metrics/stats/__init__.py
+++ examples/python/kafka/metrics/stats/__init__.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import
 
-from kafka.metrics.stats.avg import Avg
-from kafka.metrics.stats.count import Count
-from kafka.metrics.stats.histogram import Histogram
-from kafka.metrics.stats.max_stat import Max
-from kafka.metrics.stats.min_stat import Min
-from kafka.metrics.stats.percentile import Percentile
-from kafka.metrics.stats.percentiles import Percentiles
-from kafka.metrics.stats.rate import Rate
-from kafka.metrics.stats.sensor import Sensor
-from kafka.metrics.stats.total import Total
+from examples.python.kafka.metrics.stats.avg import Avg
+from examples.python.kafka.metrics.stats.count import Count
+from examples.python.kafka.metrics.stats.histogram import Histogram
+from examples.python.kafka.metrics.stats.max_stat import Max
+from examples.python.kafka.metrics.stats.min_stat import Min
+from examples.python.kafka.metrics.stats.percentile import Percentile
+from examples.python.kafka.metrics.stats.percentiles import Percentiles
+from examples.python.kafka.metrics.stats.rate import Rate
+from examples.python.kafka.metrics.stats.sensor import Sensor
+from examples.python.kafka.metrics.stats.total import Total
 
 __all__ = [
     'Avg', 'Count', 'Histogram', 'Max', 'Min', 'Percentile', 'Percentiles',
