4794243eda2d3d5c38737a5f47d9113a8669ef51
Andrew Chen <andrew0704us@gmail.com>
2018-04-07 22:58:02 -0600

    [Fix] fix the import problems

--- examples/python/kafka/partitioner/__init__.py
+++ examples/python/kafka/partitioner/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
-from kafka.partitioner.default import DefaultPartitioner
-from kafka.partitioner.hashed import HashedPartitioner, Murmur2Partitioner, LegacyPartitioner
-from kafka.partitioner.roundrobin import RoundRobinPartitioner
+from examples.python.kafka.partitioner.default import DefaultPartitioner
+from examples.python.kafka.partitioner.hashed import HashedPartitioner, Murmur2Partitioner, LegacyPartitioner
+from examples.python.kafka.partitioner.roundrobin import RoundRobinPartitioner
 
 __all__ = [
     'DefaultPartitioner', 'RoundRobinPartitioner', 'HashedPartitioner',
