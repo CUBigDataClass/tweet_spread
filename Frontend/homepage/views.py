51142ca918ab35ef886a92648b66a0173e2c9431
Sharan Srivatsa <shsr7814@colorado.edu>
2018-04-21 14:57:56 -0600

    changed parser

--- Frontend/homepage/views.py
+++ Frontend/homepage/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from . import process_search
-from django.utils import simplejson
+import json
 
 
 
 def index(request):
 	my_json = [{"coords": [-63.2425206, -32.4079042],"frequency": 9},{"coords": [12.57994249, 55.68087366],"frequency": 3}];
-	js_data = simplejson.dumps(my_json)
+	js_data = json.dump(my_json)
 	return render(request, 'homepage/header.html', {'my_json': js_data});
 
 
