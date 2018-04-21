6324fc5482936d1c598a173c1e91328dfc5e9c37
Sharan Srivatsa <shsr7814@colorado.edu>
2018-04-21 14:50:16 -0600

    fixing json input

--- Frontend/homepage/views.py
+++ Frontend/homepage/views.py
@@ -3,15 +3,9 @@ from . import process_search
 
 
 def index(request):
-	json_dummy = [{
-				    "coords": [-63.2425206, -32.4079042],
-				    "frequency": 9
-				}, {
-				    "coords": [12.57994249, 55.68087366],
-				    "frequency": 3
-				}]
+	my_json = [{"coords": [-63.2425206, -32.4079042],"frequency": 9},{"coords": [12.57994249, 55.68087366],"frequency": 3}];
 
-	return render(request, 'homepage/header.html', {'json_dummy': json_dummy})
+	return render(request, 'homepage/header.html', {'my_json': my_json});
 
 
 def home(request):
