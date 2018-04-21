526f609562d6823c6a40c9a51c17dc9e1f402ff2
Sharan Srivatsa <shsr7814@colorado.edu>
2018-04-21 17:56:32 -0600

    Initial changes

--- Frontend/homepage/views.py
+++ Frontend/homepage/views.py
@@ -5,17 +5,10 @@ import json
 
 def topic_model(request):
 	# amruta : Todo .. pass this to the front end.
-	topic_json = {1:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	2:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	3:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	4:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	5:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	6:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	7:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	8:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	9:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 },
-	10:{"word1":10, "word2":20, "word3":30, "word4":40, "word5":50, "word6":60, "word7":70, "word8":80, "word9":90, "word10":100 }
-	}
+	'''
+	var topic_json = {"t1":[{"word1":10, "word2":20, "word3":30}]}; # this is how it should look on the javascript side. 
+	'''
+	pass
 
 def index(request):
 	my_json = [{"coords": [-63.2425206, -32.4079042],"frequency": 9},{"coords": [12.57994249, 55.68087366],"frequency": 3}];
