d01e1ad8761259d34fc8a47ce7817ddfed1ce760
Sharan Srivatsa <shsr7814@colorado.edu>
2018-04-21 14:16:02 -0600

    identical pages

--- Frontend/homepage/static/homepage/charts/new-worldmap.js
+++ Frontend/homepage/static/homepage/charts/new-worldmap.js
@@ -1,5 +1,5 @@
   
-  function plot_world_map(world_map, places)
+  function plot_world_map(world_map, data)
   {
     places = {"my_place":[{
     "coords": [-63.2425206, -32.4079042],
@@ -20,9 +20,10 @@
 
     var projection = d3.geo.mercator().scale(120).translate([width/2,height/1.5])
     var path = d3.geo.path().projection(projection);
-    alert("projection is:" + projection)
-    alert("path is:" + path)
-    alert('world_map is: '+ world_map);
+    //alert("projection is:" + projection)
+    //alert("path is:" + path)
+    //alert('world_map is: '+ world_map);
+
     d3.json(world_map,function(geo_data){
       alert("geo data is: "+ geo_data.features);
       var map = svg.selectAll('path')
@@ -30,30 +31,30 @@
         .enter()
         .append('path')
         .attr('d',path)
-        .style({'fill':'#E5DBD2','stroke':'#fff','stroke-width':.6})
-        alert("map is: "+map); 
-      d3.json(places,function(data){ // Amruta just add stuff to this file. And it will plot it               
-        places = data.map(function(d){
+        .style({'fill':'#E5DBD2','stroke':'#fff','stroke-width':.6})        
+        alert("data.map is: " + geo_data.features);
+        alert("map is: " + map);
+
+      //d3.json(places,function(data){ // Amruta just add stuff to this file. And it will plot it               
+        data = data.map(function(d){
           alert("in data.map ... d is: " + d);
           return {coords:projection([+d.coords[0],+d.coords[1]]),frequency:d.frequency}
         })
         //alert("data is:" + data);
         var rScale = d3.scale.sqrt()
-          .domain(d3.extent(places,function(d){ alert("d is: "+d);alert("d.freq:" + d.frequency);return d.frequency}))
+          .domain(d3.extent(places,function(d){return d.frequency}))
           .range([2,4])
-          alert("places is:" + places);
-          alert("places.my_place: " + places.my_place);
 
           var bubble = svg.selectAll('.bubble')
-          .data(places.my_place)
+          .data(data)
           .enter()
           .append('g')
           .attr('class','bubble')
           bubble.append('circle')
-          .attr('cx',function(d){alert("d is:"+d);alert("coords is: "+d.coords);return d.coords[0]})
+          .attr('cx',function(d){return d.coords[0]})
           .attr('cy',function(d){return d.coords[1]})
           .attr('r',function(d){return rScale(d.frequency)})
-          .attr('fill','#000000') // Just change things here. For the color of what ever. and you are done.
+          .attr('fill','#F26247') // Just change things here. For the color of what ever. and you are done.
       })
-    })
+    //})
     }
