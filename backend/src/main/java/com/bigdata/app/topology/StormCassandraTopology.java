a74f4255de009be8877f3a0055f648f55b93cf9f
Chu-Sheng Ku <bamboo983@gmail.com>
2018-04-22 14:35:24 -0600

    Remove unnecessary libraries and comments

--- backend/src/main/java/com/bigdata/app/topology/StormCassandraTopology.java
+++ backend/src/main/java/com/bigdata/app/topology/StormCassandraTopology.java
@@ -18,29 +18,17 @@ import org.apache.storm.cassandra.bolt.CassandraWriterBolt;
 import com.bigdata.app.bolt.JSONParsingBolt;
 import com.bigdata.app.sentiments.SentimentBolt;
 
-import com.hmsonline.storm.cassandra.StormCassandraConstants;
-import com.hmsonline.storm.cassandra.bolt.AckStrategy;
-import com.hmsonline.storm.cassandra.bolt.CassandraBatchingBolt;
-import com.hmsonline.storm.cassandra.bolt.mapper.DefaultTupleMapper;
-//import com.hmsonline.storm.cassandra.bolt.CassandraWriterBolt;
-
-import java.util.Arrays;
-import java.util.HashMap;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class StormCassandraTopology {
 
-    private static final String CASSANDRA_KEYSPACE = "tweetanalysis";
-    private static final String CASSANDRA_COLUMN_FAMILY = "tweetSentiments";
-    private static final String CASSANDRA_ROWKEY_FIELD = "tweet";
-
     public static void main(String[] args) {
         // zookeeper hosts for the Kafka cluster
         BrokerHosts zkHosts = new ZkHosts("54.245.62.87:2181");
 
-        // Create the KafkaSpout configuartion
+        // Create KafkaSpout configuration
         // Second argument is the topic name
         // Third argument is the zookeeper root for Kafka
         // Fourth argument is consumer group id
@@ -62,20 +50,7 @@ public class StormCassandraTopology {
         // Set kafka spout
         builder.setSpout("KafkaSpout", new KafkaSpout(kafkaConfig), 1);
 
-//        Config config = new Config();
-//        String configKey = "cassandra-config";
-//        HashMap<String, Object> clientConfig = new HashMap<String, Object>();
-//        clientConfig.put(StormCassandraConstants.CASSANDRA_HOST, "172.31.21.76:9160");
-////        clientConfig.put(StormCassandraConstants.CASSANDRA_PORT, "9160");
-//        clientConfig.put(StormCassandraConstants.CASSANDRA_KEYSPACE, Arrays.asList(new String[]{"tweetanalysis"}));
-//        config.put(configKey, clientConfig);
-
-        // TODO: Create cassandra bot
-        // Create a CassandraBolt that writes to the column family and use Tuple field as the row key
-//        CassandraBatchingBolt<String, String, String> cassandraBolt = new CassandraBatchingBolt<String, String, String>(configKey,
-//                new DefaultTupleMapper(CASSANDRA_KEYSPACE, CASSANDRA_COLUMN_FAMILY, CASSANDRA_ROWKEY_FIELD));
-//        cassandraBolt.setAckStrategy(AckStrategy.ACK_ON_WRITE);
-        //Load properties file
+        // Load properties file
         Properties props = new Properties();
         try {
             InputStream is = StormCassandraTopology.class.getClassLoader().getResourceAsStream("cassandra.properties");
@@ -91,7 +66,7 @@ public class StormCassandraTopology {
         Config conf = new Config();
         conf.setDebug(true);
 
-        //Copy properies to storm config
+        // Copy properties to storm config
         for (String name : props.stringPropertyNames()) {
             conf.put(name, props.getProperty(name));
         }
@@ -102,24 +77,23 @@ public class StormCassandraTopology {
         CassandraWriterBolt cassandraBolt = new CassandraWriterBolt(async(
                 simpleQuery(cql).with(fields("tweet", "sentiment"))));
 
-
+        // Create JSON parser bolt
         builder.setBolt("json", new JSONParsingBolt()).shuffleGrouping("KafkaSpout");
 
-
+        // Create sentiment analysis bolt
         builder.setBolt("sentiment", new SentimentBolt("/home/ec2-user/tweet_spread/backend/src/main/resources/AFINN-111.txt")).shuffleGrouping("json", "stream2");
 
-        // TODO: Create cassandra bot
+        // Create Cassandra writer bolt
         builder.setBolt("cassandra-bolt", cassandraBolt, 3).shuffleGrouping("sentiment");
 
         // Submit topology for execution
         try {
-            StormSubmitter.submitTopology("StormCassandraToplogy", conf, builder.createTopology());
+            StormSubmitter.submitTopology("StormCassandraTopology", conf, builder.createTopology());
         } catch (AlreadyAliveException alreadyAliveException) {
             System.out.println(alreadyAliveException);
         } catch (InvalidTopologyException invalidTopologyException) {
             System.out.println(invalidTopologyException);
         } catch (AuthorizationException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
