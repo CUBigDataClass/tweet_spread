84c5e7f4da2c571106dd77c828b4dfdf5cbaf9c4
amruta <hail.radical@gmail.com>
2018-04-28 15:38:47 -0600

    autorefresh

--- Frontend/homepage/process_search.py
+++ Frontend/homepage/process_search.py
@@ -23,7 +23,7 @@ def connect_kafka(topic):
 def get_sentiment(topic):
 	cluster = Cluster(['54.245.62.87'])
 	session = cluster.connect()
-	result = session.execute("select hashtag from tweetanalysis.sentiments where hashtag=english;")
+	result = session.execute("select * from tweetanalysis.sentiments where hashtag='english';")
 	cluster.shutdown()
 	final_res = []
 	for elem in result:
