48dd001bf7509d90bf07eac7b697dd5e8720f366
Sharan Srivatsa <shsr7814@colorado.edu>
2018-04-28 12:45:50 -0600

    changed return type

--- Frontend/homepage/static/homepage/charts/chart3.js
+++ Frontend/homepage/static/homepage/charts/chart3.js
@@ -1,5 +1,24 @@
-
+convert_milestone_to_datapoints(milestone_json) {
+    var datapoints =  [
+        { x: new Date(2017, 0, 3), y: 450 },
+        { x: new Date(2017, 0, 4), y: 414},
+        { x: new Date(2017, 0, 5), y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
+        { x: new Date(2017, 0, 6), y: 460 },
+        { x: new Date(2017, 0, 7), y: 450 },
+        { x: new Date(2017, 0, 8), y: 500 },
+        { x: new Date(2017, 0, 9), y: 480 },
+        { x: new Date(2017, 0, 10), y: 480 },
+        { x: new Date(2017, 0, 11), y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
+        { x: new Date(2017, 0, 12), y: 500 },
+        { x: new Date(2017, 0, 13), y: 480 },
+        { x: new Date(2017, 1, 14), y: 510 }
+    ];
+    return datapoints;
+}
 function plot_milestones() {
+    milestone_json = {[{year:2017, month:0, day:3, count:450}]}
+    var datapoints = convert_milestone_to_datapoints(milestone_json)
+
     var chart = new CanvasJS.Chart("chart2", {
         animationEnabled: true,
         theme: "light2",
@@ -11,7 +30,7 @@ function plot_milestones() {
         },
         data: [{        
             type: "line",       
-            dataPoints: [
+            /*dataPoints: [
                 { x: new Date(2017, 0, 3), y: 450 },
                 { x: new Date(2017, 0, 4), y: 414},
                 { x: new Date(2017, 0, 5), y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
@@ -24,7 +43,8 @@ function plot_milestones() {
                 { x: new Date(2017, 0, 12), y: 500 },
                 { x: new Date(2017, 0, 13), y: 480 },
                 { x: new Date(2017, 1, 14), y: 510 }
-            ]
+            ]*/
+            dataPoints: datapoints;
         }]
     });
     chart.render();
