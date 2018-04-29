12c0f1626fe9372ce10c7539bab3d12459a706d8
amruta <hail.radical@gmail.com>
2018-04-28 22:06:54 -0600

    s

--- Frontend/homepage/views.py
+++ Frontend/homepage/views.py
@@ -24,7 +24,7 @@ def home(request):
 		query = request.GET['search']
 		mode = "Fetched from ajax"
 		sentiment = process_search.get_sentiment(query)
-		return HttpResponse(json.dumps(sentiment), content_type='application/json')
+		return HttpResponse(json.loads(sentiment), content_type='application/json')
 
 	if request.method == 'GET':
 		query = request.GET['search']
