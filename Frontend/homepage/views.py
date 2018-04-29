7ffc9ee563d0f9d45aee248e254c362ddbbd9b24
Andrew Chen <andrew0704us@gmail.com>
2018-04-29 05:24:08 -0600

    [Test] test1

--- Frontend/homepage/views.py
+++ Frontend/homepage/views.py
@@ -42,7 +42,7 @@ def home(request):
 		if query:
 			mode = "Fetched from cassandra"
 			sentiment = process_search.get_sentiment(query)
-			geoparsed = process_search.get_geoparse(query)
+			# geoparsed = process_search.get_geoparse(query)
 			#top_tweets_1, top_tweets_2 = process_search.get_top_tweets(query)
 			topic_models = process_search.get_topics(query)
 			if sentiment is None:
@@ -51,7 +51,7 @@ def home(request):
 				#top_tweets_1, top_tweets_2 = process_search.get_top_tweets(query)
 				time.sleep(3)
 				sentiment = process_search.get_sentiment(query)
-				geoparsed = process_search.get_geoparse(query)
+				# geoparsed = process_search.get_geoparse(query)
 			#return render(request, 'homepage/search.html', {'query': query, 'sentiment': sentiment, 'mode': mode,
 			#'top_tweets_1': top_tweets_1, 'top_tweets_2': top_tweets_2, 'topic_models': topic_models})
 			return render(request, 'homepage/search.html', {'query': query, 'sentiment': sentiment, 'mode': mode,
