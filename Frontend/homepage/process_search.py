bb8f70978da880c6bf03562770d7e474451c7053
amruta <hail.radical@gmail.com>
2018-04-29 04:21:20 -0600

    gp

--- Frontend/homepage/process_search.py
+++ Frontend/homepage/process_search.py
@@ -63,8 +63,8 @@ def get_geoparse(topic):
 	for elem in result:
 		for i in elem:
 			for j in i:
-				j = re.sub("^\"\{ironman\}\"", "\"" + str(counter) + "\"", j)
-				final_res = final_res + str(j) + ","
+				k = re.sub("^\"\{ironman\}\"", "\"" + str(counter) + "\"", j)
+				final_res = final_res + str(k) + ","
 				counter += 1
 	return final_res
 
