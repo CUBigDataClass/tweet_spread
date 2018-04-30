56ef42193fdb3bef45074fef1171477b0bb45b90
sayalisonawane <sayalisonawane27@gmail.com>
2018-04-30 11:04:12 -0600

    fixed query

--- backend/src/main/java/com/bigdata/app/cassandra/Query.java
+++ backend/src/main/java/com/bigdata/app/cassandra/Query.java
@@ -2,31 +2,30 @@ package com.bigdata.app.cassandra;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
-import java.util.Date;
 
 public class Query {
 
     public static void main(String args[]) {
 
-        String date= "Sat Apr 28 18:20:57 +0000 2018";
-        Date day = Date(date);
-        System.out.println(day);
+//        String date= "Sat Apr 28 18:20:57 +0000 2018";
+//        Date day = Date(date);
+//        System.out.println(day);
 
-//        //query
-//        String query = "update tweetanalysis.sentiments set positive_sentiments = positive_sentiments + 1, "+
-//                "negative_sentiments = negative_sentiments + 2, neutral_sentiments = neutral_sentiments + 3 "+
-//                "where hashtag = 'hash1';"
-//        ;
-//
-//        //Creating Cluster object
-//        Cluster cluster = Cluster.builder().addContactPoint("54.245.62.87").build();
-//
-//        //Creating Session object
-//        Session session = cluster.connect("tweetanalysis");
-//
-//        //Executing the query
-//        session.execute(query);
-//
-//        System.out.println("Changes done");
+        //query
+        String query = "update tweetanalysis.sentiments set positive_sentiments = positive_sentiments + 1, "+
+                "negative_sentiments = negative_sentiments + 2, neutral_sentiments = neutral_sentiments + 3 "+
+                "where hashtag = 'hash1';"
+        ;
+
+        //Creating Cluster object
+        Cluster cluster = Cluster.builder().addContactPoint("54.245.62.87").build();
+
+        //Creating Session object
+        Session session = cluster.connect("tweetanalysis");
+
+        //Executing the query
+        session.execute(query);
+
+        System.out.println("Changes done");
     }
 }
