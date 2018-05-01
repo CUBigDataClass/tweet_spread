5e48b2f696c9d4f2004f5dc564110772ab374d76
amruta <hail.radical@gmail.com>
2018-05-01 10:24:41 -0600

    milestones

--- Frontend/homepage/process_search.py
+++ Frontend/homepage/process_search.py
@@ -73,9 +73,9 @@ def get_milestones(topic):
 	for month in range(0, 11):
 		for day in range(1, 31):
 			for hour in range(0, 23):
-				query = "select count from hashtag_milestones where hashtag = techcrunch and" \
-				        " year = 2018 and month =" + str(month) +" and day =" + str(day)+\
-				        " and hour =" + str(hour)
+				query = "select count from tweetanalysis.hashtag_milestones where " \
+				        "hashtag = techcrunch and year = 2018 and month =" + str(month) + \
+				        " and day =" + str(day)+ " and hour =" + str(hour)
 				milestone_json.append("query fired")
 				result = session.execute(query)
 				if result:
