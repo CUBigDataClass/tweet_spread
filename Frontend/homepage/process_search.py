6fa0c8126d1ac6b96a5ba388c94d64a1f8ea8411
amruta <hail.radical@gmail.com>
2018-05-01 13:41:38 -0600

    milestones

--- Frontend/homepage/process_search.py
+++ Frontend/homepage/process_search.py
@@ -96,16 +96,6 @@ def get_milestones(topic, mode = 0):
 		milestone_json = milestone_json.strip(",")
 		milestone_json = "[" + milestone_json + "]"
 	else:
-		for hour in range(23, -1, -1):
-			query = "select count from tweetanalysis.hashtag_milestones where " \
-			        "hashtag = '" + topic + "' and year = 2018 and month = 4 and " \
-			                                "day = 1  and hour =" + str(hour)
-			result = session.execute(query)
-			if result:
-				for elem in result:
-					result_dict = {"x": "new Date(2018, 4, 1, " + str(hour) + ")", "y": elem[0]}
-					milestones.append(result_dict)
-
 		for day in range(31, 20, -1):
 			for hour in range(23, -1, -1):
 				query = "select count from tweetanalysis.hashtag_milestones where " \
@@ -114,7 +104,7 @@ def get_milestones(topic, mode = 0):
 				result = session.execute(query)
 				if result:
 					for elem in result:
-						result_dict = {"x": "new Date(2018, 3, " + str(day) + " , " + str(hour) + ")", "y": elem[0]}
+						result_dict = "{\"x\": new Date(2018, 3, " + str(day) + ", " + str(hour)+" ), \"y\": " + str(elem[0]) + "}"
 						milestones.append(result_dict)
 		cluster.shutdown()
 		return milestones
