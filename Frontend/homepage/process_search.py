229e688b64d2353e5d1cd35e77dd6e08cfcdf04a
amruta <hail.radical@gmail.com>
2018-04-30 22:19:56 -0600

    sentiment

--- Frontend/homepage/process_search.py
+++ Frontend/homepage/process_search.py
@@ -34,7 +34,7 @@ def get_sentiment(topic):
 				final_res.append(elem[i])
 		final_res = [(x/float(sum(final_res)))*100 for x in final_res]
 		my_json = str([{'y': final_res[0], 'label': "POS"}, {'y': final_res[1], 'label': "NEG"}, {'y': final_res[2], 'label': "NEU"}])
-		return my_json
+		return final_res
 	else:
 		return None
 
